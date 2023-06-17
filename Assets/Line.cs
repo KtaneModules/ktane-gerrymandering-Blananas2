560162a8f8b8bcf53dc0d50a6780b48fe26ea178
Blananas2 <blananas2@gmail.com>
2023-06-17 18:47:37 -0400

    line drawing algorithm (epiku)

---
+++ Assets/Line.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using KModkit;
+
+public class Line : MonoBehaviour {
+
+    public KMSelectable[] Blocs;
+	public SpriteRenderer[] Slots;
+	public Sprite[] Lines;
+
+    bool holding = false;
+	int number = -1;
+	int[] grid = { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 
+				   -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 
+				   -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 
+				   -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 
+				   -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 
+				   -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 
+				   -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 
+				   -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 
+				   -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1  };
+
+    void Start () {
+        foreach (KMSelectable Bloc in Blocs) {
+            Bloc.OnInteract += delegate () { holding = true; number += 1; BlocUpdate(Bloc); return false; };
+			Bloc.OnHighlight += delegate { BlocUpdate(Bloc); };
+            Bloc.OnInteractEnded += delegate { holding = false; };
+        }
+    }
+
+	void BlocUpdate(KMSelectable Bloc) {
+        for (int b = 0; b < Blocs.Length; b++) {
+			if (Bloc == Blocs[b]) {
+				UpdateLines(b);
+			}
+		}
+    }
+
+	void UpdateLines(int x) {
+		if (!holding) { return; }
+		grid[x] = number;
+		for (int b = 0; b < 117; b++) {
+			if (grid[b] == -1) { continue; }
+			int total = 0;
+			if (b / 13 != 0) {
+				if (grid[b-13] == grid[b]) {
+					total += 1;
+				}
+			}
+			if (b % 13 != 0) {
+				if (grid[b-1] == grid[b]) {
+					total += 2;
+				}
+			}
+			if (b / 13 != 8) {
+				if (grid[b+13] == grid[b]) {
+					total += 4;
+				}
+			}
+			if (b % 13 != 12) {
+				if (grid[b+1] == grid[b]) {
+					total += 8;
+				}
+			}
+			Slots[b].sprite = Lines[total];
+		}
+	}
+
+}
